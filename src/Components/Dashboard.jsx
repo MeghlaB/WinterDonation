cdf8b32a2c19ede7982e7590c12127b2e82c7b13
MeghlaB <meghlab964@gmail.com>
2024-11-21 20:51:56 +0600

    image added

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -5,23 +5,23 @@ import { NavLink } from 'react-router-dom'
 export default function Dashboard() {
   const {user,logout} = useContext(AuthContext)
   return (
-    <div>
-     <div className="w-11/12 border border-slate-800 mx-auto my-5 rounded-2xl ">
-  <div className="hero-content text-center">
-    <div className="max-w-md">
-      <h1 className="text-5xl font-bold py-4"> Welcome {user?.displayName}  🌸</h1>
-     <div className='py-4'>
-        <img className='w-30 mx-auto' src={user?.photoURL} alt="" />
-        <p className='text-xl lg:text-2xl font-bold'>Email: {user?.email}</p>
-        <p className='text-xl lg:text-2xl font-bold'>Name:{user?.displayName}</p>
-     </div>
-      <div className='flex flex-col space-y-4'>
-      <NavLink to={'/update'} className="btn btn-primary">UpdateProfile</NavLink>
-      </div>
+    <div className='my-2'>
+     <div className=" container mx-auto  border text-center flex items-center  justify-center border-slate-800  py-10 rounded-2xl ">
+      <div className=" text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold py-4"> Welcome {user?.displayName}  🌸</h1>
+        <div className='py-4 '>
+            <img className='w-30  border p-4 rounded-md mx-auto' src={user?.photoURL} alt="" />
+            <p className='text-xl lg:text-2xl font-bold'>Email: {user?.email}</p>
+            <p className='text-xl lg:text-2xl font-bold'>Name:{user?.displayName}</p>
+        </div>
+          <div className='flex flex-col space-y-4'>
+          <NavLink to={'/update'} className="btn btn-primary">UpdateProfile</NavLink>
+          </div>
+          
+        </div>
       
-    </div>
-  
-  </div>
+      </div>
 </div>
     </div>
   )
